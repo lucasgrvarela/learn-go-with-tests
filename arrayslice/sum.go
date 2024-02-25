b9ca4743cf8fd5d76a9ac6fa7c903734fbf5d9f0
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2024-02-24 22:45:47 -0300

    add on arrayslice pkg SumAll, SumAllTails functions and tests

--- arrayslice/sum.go
+++ arrayslice/sum.go
@@ -7,3 +7,26 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(slices ...[]int) []int {
+	var sums []int
+	for _, slice := range slices {
+		sums = append(sums, Sum(slice))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
--- arrayslice/sum_test.go
+++ arrayslice/sum_test.go
@@ -1,9 +1,11 @@
 package arrayslice
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
@@ -14,5 +16,31 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
 
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2, 4}, []int{0, 9, 4})
+	want := []int{7, 13}
+	checkSums(t, got, want)
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
+}
+
+func checkSums(t testing.TB, got, want []int) {
+	t.Helper()
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
