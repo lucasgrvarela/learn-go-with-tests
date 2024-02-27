7b792ec8f7f55020b56038d666633e3387717480
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2024-02-26 22:51:42 -0300

    finishing off the work on structs pkg, add shapes interface, add table driven tests

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,37 @@
 package structs
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Shape interface {
+	Area() float64
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
 }
